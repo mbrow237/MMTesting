67964e1e08b293ec98485e6e83ea423b9733b777
Melvin <melvinbrown1993@gmail.com>
2019-01-04 18:08:47 -0500

    Basic Sound effects

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var megaManPort = document.getElementById('MegaMan');
+var audio = document.getElementsByTagName("audio")[0];
 var megaManPortIndex = 0;
 
 function hoverOverRobot(x){
@@ -6,34 +7,42 @@ function hoverOverRobot(x){
     switch (megaManPortIndex) {
         case 1:
             megaManPort.src = "styles/images/portraits/megaman/top-left.png";
+            audio.play();
         break;
 
         case 2:
             megaManPort.src = "styles/images/portraits/megaman/top-mid.png";
+            audio.play();
         break;
 
         case 3:
             megaManPort.src = "styles/images/portraits/megaman/top-right.png";
+            audio.play();
         break;
 
         case 4:
             megaManPort.src = "styles/images/portraits/megaman/left.png";
+            audio.play();
         break;
 
         case 5:
             megaManPort.src = "styles/images/portraits/megaman/right.png";
+            audio.play();
         break;
 
         case 6:
             megaManPort.src = "styles/images/portraits/megaman/bot-left.png";
+            audio.play();
         break;
 
         case 7:
             megaManPort.src = "styles/images/portraits/megaman/bot-mid.png";
+            audio.play();
         break;
 
         case 8:
             megaManPort.src = "styles/images/portraits/megaman/bot-right.png";
+            audio.play();
         break;
 
         case 0: 
